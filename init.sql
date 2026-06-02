4bd2339c03e7ab35638f63ec698ba2b6a4c9baef
Anton Muravev <tsb99x@gmail.com>
2026-06-02 23:08:18 +0300

    Format init.sql with sqruff

--- init.sql
+++ init.sql
@@ -5,30 +5,30 @@ CREATE TABLE account (
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
-INSERT INTO account(id)
+INSERT INTO account (id)
 VALUES ('00000000-0000-0000-0000-000000000000');
 
 -- Main transfer table with constraints, indexes, checks and triggers
 
 CREATE TABLE transfer (
-    source UUID NOT NULL REFERENCES account(id),
+    source UUID NOT NULL REFERENCES account (id),
     index INTEGER NOT NULL,
-    destination UUID NOT NULL REFERENCES account(id),
+    destination UUID NOT NULL REFERENCES account (id),
     amount DECIMAL NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
 
     PRIMARY KEY (source, index),
-    CONSTRAINT transfer_positive_amount CHECK (amount > 0),
-    CONSTRAINT transfer_not_same_account CHECK (source != destination)
+    constraint TRANSFER_POSITIVE_AMOUNT CHECK (amount > 0),
+    constraint TRANSFER_NOT_SAME_ACCOUNT CHECK (source != destination)
 );
 
-CREATE INDEX transfer_source ON transfer(source);
-CREATE INDEX transfer_destination ON transfer(destination);
-CREATE INDEX transfer_created_at ON transfer(created_at);
+CREATE INDEX transfer_source ON transfer (source);
+CREATE INDEX transfer_destination ON transfer (destination);
+CREATE INDEX transfer_created_at ON transfer (created_at);
 
 -- Tricky function to remove SQL aggregation mess from service application
 
-CREATE FUNCTION account_metadata(UUID[])
+CREATE FUNCTION account_metadata (UUID [])
 RETURNS TABLE (id UUID, balance NUMERIC, next_transfer_index INTEGER) AS $BODY$
     BEGIN
         RETURN QUERY
@@ -59,11 +59,11 @@ RETURNS TABLE (id UUID, balance NUMERIC, next_transfer_index INTEGER) AS $BODY$
                    GROUP BY source) o ON o.id = a.id;
     END;
 $BODY$
-LANGUAGE PLPGSQL;
+LANGUAGE PLPGSQL ;
 
 -- Trigger to check source balance exactly BEFORE insertion of new transfer
 
-CREATE FUNCTION check_source_balance()
+CREATE FUNCTION check_source_balance ()
 RETURNS TRIGGER AS $BODY$
     DECLARE
         source_balance DECIMAL;
@@ -84,15 +84,15 @@ RETURNS TRIGGER AS $BODY$
         RETURN NEW;
     END;
 $BODY$
-LANGUAGE PLPGSQL;
+LANGUAGE PLPGSQL ;
 
 CREATE TRIGGER transfer_check_source_balance
 BEFORE INSERT ON transfer
-FOR EACH ROW EXECUTE PROCEDURE check_source_balance();
+FOR EACH ROW EXECUTE PROCEDURE check_source_balance () ;
 
 -- Trigger to check indexing (order) of transfer BEFORE insertion of new one
 
-CREATE FUNCTION check_indexing()
+CREATE FUNCTION check_indexing ()
 RETURNS TRIGGER AS $BODY$
     DECLARE
         expected_index INTEGER;
@@ -109,15 +109,15 @@ RETURNS TRIGGER AS $BODY$
         RETURN NEW;
     END;
 $BODY$
-LANGUAGE PLPGSQL;
+LANGUAGE PLPGSQL ;
 
 CREATE TRIGGER transfer_check_indexing
 BEFORE INSERT ON transfer
-FOR EACH ROW EXECUTE PROCEDURE check_indexing();
+FOR EACH ROW EXECUTE PROCEDURE check_indexing () ;
 
 -- Naive account initialization to get rid of unnecessary round-trips to DB
 
-CREATE FUNCTION init_account(UUID, NUMERIC)
+CREATE FUNCTION init_account (UUID, NUMERIC)
 RETURNS VOID AS $BODY$
     DECLARE
         next_index INTEGER;
@@ -136,29 +136,24 @@ RETURNS VOID AS $BODY$
         END IF;
     END;
 $BODY$
-LANGUAGE PLPGSQL;
+LANGUAGE PLPGSQL ;
 
--- Testing view to see all accounts and their metadata, not used in service directly
+-- Testing view to see all accounts and their metadata
+-- This view is not used in service directly!
 
 CREATE VIEW combined_metadata AS
-    SELECT
-    	a.id,
-        COALESCE(incoming_sum, 0) - COALESCE(outgoing_sum, 0) AS balance,
-        COALESCE(last_index + 1, 0) AS next_transfer_index
-    -- Get all account ids
-    FROM (SELECT
-              id
-          FROM account) a
-    -- Join with sum of all incoming transfers
-    LEFT JOIN (SELECT
-                   destination AS id,
-                   SUM(amount) AS incoming_sum
-               FROM transfer
-               GROUP BY destination) i ON i.id = a.id
-    -- Join with sum of all outgoing transfers and last transfer index
-    LEFT JOIN (SELECT
-                   source AS id,
-                   SUM(amount) AS outgoing_sum,
-                   MAX(index) AS last_index
-               FROM transfer
-               GROUP BY source) o ON o.id = a.id;
+SELECT a.id,
+COALESCE (incoming_sum, 0) - COALESCE (outgoing_sum, 0) AS balance,
+COALESCE (last_index + 1, 0) AS next_transfer_index
+-- Get all account ids
+FROM (SELECT id FROM account) a
+-- Join with sum of all incoming transfers
+LEFT JOIN (
+SELECT destination AS id, SUM (amount) AS incoming_sum
+FROM transfer GROUP BY destination
+) i ON i.id = a.id
+-- Join with sum of all outgoing transfers and last transfer index
+LEFT JOIN (
+SELECT source AS id, SUM (amount) AS outgoing_sum, MAX (index) AS last_index
+FROM transfer
+GROUP BY source) o ON o.id = a.id ;
